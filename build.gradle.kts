f6253f72fa0ed3b29780e017b7d4c57cfd561aa3
Score2 <836696023@qq.com>
2021-02-03 21:16:26 +0800

    Complete frames.

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = "me.scoretwo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    mavenLocal()
+    maven("http://repo.iroselle.com/snapshots/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("commons-cli:commons-cli:1.4")
+    implementation("org.jetbrains.kotlin:kotlin-script-util:1.4.21")
+    implementation("org.jetbrains.kotlin:kotlin-compiler:1.4.21")
+    compileOnly("me.scoretwo:FastScript-common:1.0.1-SNAPSHOT")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    dependencies {
+        exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
+        exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib-common"))
+
+        include(dependency("commons-cli:commons-cli:1.4"))
+        include(dependency("org.jetbrains.kotlin:kotlin-script-util:1.4.21"))
+        include(dependency("org.jetbrains.kotlin:kotlin-compiler:1.4.21"))
+    }
+}
